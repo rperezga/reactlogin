d6c3afb2df34d443e71531fc925e14784b0759b8
Roger Perez <rperezga@gmail.com>
2019-01-16 14:05:28 +0000

    Updates

---
+++ precache-manifest.c631cbde0c6e9e71dd008c82b26c1d3e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa87a0f6dd2aa6fbb0f",
+    "url": "/reactlogin/static/css/main.00f6121e.chunk.css"
+  },
+  {
+    "revision": "aaa87a0f6dd2aa6fbb0f",
+    "url": "/reactlogin/static/js/main.aaa87a0f.chunk.js"
+  },
+  {
+    "revision": "1bf997570ce5b2b66dea",
+    "url": "/reactlogin/static/css/1.59a8ccc0.chunk.css"
+  },
+  {
+    "revision": "1bf997570ce5b2b66dea",
+    "url": "/reactlogin/static/js/1.1bf99757.chunk.js"
+  },
+  {
+    "revision": "ce02666905d11efc263b",
+    "url": "/reactlogin/static/js/runtime~main.ce026669.js"
+  },
+  {
+    "revision": "591cc98f43cbb68c10ddf345cb2d164e",
+    "url": "/reactlogin/static/media/u2f_usb.591cc98f.png"
+  },
+  {
+    "revision": "f10c9db1d9eb441f0bdaffce5bb672a1",
+    "url": "/reactlogin/static/media/yubikeyDemo.f10c9db1.png"
+  },
+  {
+    "revision": "c8c027005764d43d83566d88f53844c9",
+    "url": "/reactlogin/static/media/checkbox-sign-in-widget@2x.c8c02700.png"
+  },
+  {
+    "revision": "eea28db2b98f4df40b5608a54d0b9131",
+    "url": "/reactlogin/static/media/sign-on-widget-spinner.eea28db2.gif"
+  },
+  {
+    "revision": "c837a09b25ad85d682483da65dd6420e",
+    "url": "/reactlogin/static/media/radiobutton-01.c837a09b.png"
+  },
+  {
+    "revision": "7e72002067c9047c205fdb3521176196",
+    "url": "/reactlogin/static/media/checkbox-01.7e720020.png"
+  },
+  {
+    "revision": "d5554fe41aed23e1e49f249f2f6bcfaa",
+    "url": "/reactlogin/static/media/checkbox-01@2x.d5554fe4.png"
+  },
+  {
+    "revision": "dc47af40165cadf418a95b6a06964b2e",
+    "url": "/reactlogin/static/media/radiobutton-01@2x.dc47af40.png"
+  },
+  {
+    "revision": "6225f3ca44b83090833064727a09cc95",
+    "url": "/reactlogin/static/media/montserrat-light-webfont.6225f3ca.woff"
+  },
+  {
+    "revision": "8f2822b73b5f9c106c6f2e0db820bcbb",
+    "url": "/reactlogin/static/media/montserrat-regular-webfont.8f2822b7.woff"
+  },
+  {
+    "revision": "db28723126138387cdf40680e6e0fa5d",
+    "url": "/reactlogin/static/media/okticon.db287231.woff"
+  },
+  {
+    "revision": "a560e17e7ab1ae0cc271da8cf0c62d8a",
+    "url": "/reactlogin/static/media/okticon.a560e17e.eot"
+  },
+  {
+    "revision": "5e1f49dda77e01218444c76678856d3d",
+    "url": "/reactlogin/static/media/okticon.5e1f49dd.ttf"
+  },
+  {
+    "revision": "a11330db59d1e0d7b0935e86754b86be",
+    "url": "/reactlogin/static/media/montserrat-light-webfont.a11330db.eot"
+  },
+  {
+    "revision": "70df5da69722ec23336c227bf131d5ab",
+    "url": "/reactlogin/static/media/montserrat-light-webfont.70df5da6.ttf"
+  },
+  {
+    "revision": "362ffe720ba40be359302cab7f83c517",
+    "url": "/reactlogin/static/media/montserrat-regular-webfont.362ffe72.eot"
+  },
+  {
+    "revision": "69eac499e9311b03ed69199e62ea962e",
+    "url": "/reactlogin/static/media/montserrat-regular-webfont.69eac499.ttf"
+  },
+  {
+    "revision": "432df6c2318e100c9bf4b820be0c66e6",
+    "url": "/reactlogin/static/media/okticon.432df6c2.svg"
+  },
+  {
+    "revision": "8cde2a0e8a4a698a32a3b1c295b9fa8b",
+    "url": "/reactlogin/static/media/montserrat-light-webfont.8cde2a0e.svg"
+  },
+  {
+    "revision": "6fedfc64a7bfbbb272c4e4d3b8597244",
+    "url": "/reactlogin/static/media/montserrat-regular-webfont.6fedfc64.svg"
+  },
+  {
+    "revision": "cf4155a7f01e3b26f52fbcd609aa55a3",
+    "url": "/reactlogin/index.html"
+  }
+];
